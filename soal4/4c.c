049cdfb371df50840c0ef2792e75f5f3eae130bf
codestellations <erlindaargyt@gmail.com>
2020-04-02 10:28:26 +0700

    Soal 4c done and deleted executables

---
+++ soal4/4c.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<sys/wait.h>
+
+int main()
+{
+	int fd1[2];
+
+	pid_t p, child, child1;
+
+	if (pipe(fd1)==-1)
+	{
+		fprintf(stderr, "Pipe Failed" );
+		return 1;
+	}
+
+	p = fork();
+
+	if (p < 0)
+	{
+		fprintf(stderr, "fork Failed" );
+		return 1;
+	}
+
+  if (p == 0){
+    dup2(fd1[1], 1);
+    close(fd1[0]);
+    close(fd1[1]);
+
+    char *argv[] = {"ls", NULL};
+    execv("/bin/ls", argv);
+  }
+
+  else{
+    dup2(fd1[0], 0);
+    close(fd1[0]);
+    close(fd1[1]);
+
+    char *argv[] = {"wc", "-l", NULL};
+    execv("/usr/bin/wc", argv);
+  }
+}
