c8980b3a9ccb938388e17be2fe3ecaa120e8c05f
liizzasulistio <58472359+liizzasulistio@users.noreply.github.com>
2020-04-10 16:52:45 +0700

    Create tapclient.c

---
+++ soal2/tapclient.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <termios.h>
+#define PORT 8080
+
+
